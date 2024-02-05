22b106882698d7eaada4027e6d7cfea020b94eef
Elene Qaldani <eleneqaldani1@gmail.com>
2024-02-05 14:58:28 +0400

    finished the users list page

--- client/src/pages/AdminPages/UserList/UserListPage.jsx
+++ client/src/pages/AdminPages/UserList/UserListPage.jsx
@@ -1,15 +1,30 @@
-import { Link } from "react-router-dom";
-import { FaTrash, FaTimes, FaCheck } from "react-icons/fa";
-import { Container } from "@mui/material";
 import Message from "../../../components/Message";
+import {
+  Container,
+  IconButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+  useTheme,
+} from "@mui/material";
+
 import Loader from "../../../components/Loader";
 import {
   useGetUsersQuery,
   useDeleteUserMutation,
 } from "../../../slices/usersApiSlice";
 import { toast } from "react-toastify";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import CheckIcon from "@mui/icons-material/Check";
+import ClearIcon from "@mui/icons-material/Clear";
 
 const UserListPage = () => {
+  const theme = useTheme();
   const { data: users, refetch, isLoading, error } = useGetUsersQuery();
 
   const [deleteUser, { isLoading: loadingDelete }] = useDeleteUserMutation();
@@ -27,57 +42,94 @@ const UserListPage = () => {
   };
 
   return (
-    <section>
-      <Container>
-        <h1>Users</h1>
+    <Container component={"section"} maxWidth={false}>
+      <Typography
+        variant="h1"
+        fontWeight={700}
+        fontSize={"1.3rem"}
+        mb={"2rem"}
+        color={theme.palette.secondary.main}
+      >
+        Users
+      </Typography>
 
-        {loadingDelete && <Loader />}
-
-        {isLoading ? (
-          <Loader />
-        ) : error ? (
-          <Message>{error}</Message>
-        ) : (
-          <table>
-            <thead>
-              <tr>
-                <th>Id</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Admin</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
+      {loadingDelete && <Loader />}
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message>{error}</Message>
+      ) : (
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Id</TableCell>
+                <TableCell align="right">Name</TableCell>
+                <TableCell align="right">Email</TableCell>
+                <TableCell align="right">Admin</TableCell>
+                <TableCell align="right"></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
               {users.map((user) => (
-                <tr key={user._id}>
-                  <td>{user._id}</td>
-                  <td>
-                    <Link to={`admin/user/${user._id}`}>{user.name}</Link>
-                  </td>
-                  <td>
-                    <a href={`mailto:${user.email}`}>{user.email}</a>
-                  </td>
-                  <td>${user.totalPrice}</td>
-                  <td>
+                <TableRow
+                  key={user._id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {user._id}
+                  </TableCell>
+
+                  <TableCell align="right">
+                    <Typography
+                      variant="body1"
+                      color={theme.palette.secondary.main}
+                      // component={Link}
+                      // to={`admin/user/${user._id}`}
+                      // sx={{ textDecoration: "none" }}
+                    >
+                      {user.name}
+                    </Typography>
+                  </TableCell>
+
+                  <TableCell align="right">
+                    <Typography
+                      component={"a"}
+                      href={`mailto:${user.email}`}
+                      color={theme.palette.secondary.main}
+                    >
+                      {user.email}
+                    </Typography>
+                  </TableCell>
+
+                  <TableCell align="right">
                     {user.isAdmin ? (
-                      <FaCheck style={{ color: "green" }} />
+                      <CheckIcon
+                        style={{ color: "green", fontSize: "1.2rem" }}
+                      />
                     ) : (
-                      <FaTimes style={{ color: "red" }} />
+                      <ClearIcon sx={{ color: "red", fontSize: "1.2rem" }} />
                     )}
-                  </td>
-                  <td>
-                    <button onClick={() => deleteHandler(user._id)}>
-                      <FaTrash />
-                    </button>
-                  </td>
-                </tr>
+                  </TableCell>
+
+                  <TableCell align="right">
+                    <IconButton
+                      onClick={() => deleteHandler(user._id)}
+                      sx={{
+                        color: theme.palette.secondary.main,
+                        fontSize: "1.2rem",
+                      }}
+                    >
+                      <DeleteForeverIcon />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
               ))}
-            </tbody>
-          </table>
-        )}
-      </Container>
-    </section>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+    </Container>
   );
 };
 export default UserListPage;
