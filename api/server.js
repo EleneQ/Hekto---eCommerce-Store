40b4e62b0f991dced96734cd8343e9a5aaaeb806
Elene Qaldani <eleneqaldani1@gmail.com>
2024-03-01 22:13:11 +0400

    changed image uploading

--- api/server.js
+++ api/server.js
@@ -33,21 +33,21 @@ app.use("/api/categories", categoryRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/checkout", stripeRoutes);
 
-//image uploads
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-
 if (process.env.NODE_ENV === "production") {
-  //set static react build folder
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static("/var/data/uploads"));
+  
   app.use(express.static(path.join(__dirname, "/client/build")));
 
-  //any non-api route will be redirected to index.html
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 } else {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
   app.get("/", (req, res) => {
-    res.send("API running...");
+    res.send("API is running....");
   });
 }
 
